76367e6028a46a7bc9d7d72c7f4889a62cc2eff5
Wisdom Dickson <dicksonwisdom005@gmail.com>
2026-08-17 10:41:47 +0100

    added query

--- src/main/java/com/wisdom/Ecom_project/repository/ProdRepository.java
+++ src/main/java/com/wisdom/Ecom_project/repository/ProdRepository.java
@@ -10,6 +10,12 @@ import java.util.List;
 @Repository
 public interface ProdRepository extends JpaRepository<Product, Integer>{
 
-    @Query()
+    @Query("SELECT p from Product p WHERE "+
+    "LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.brand) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.category) LIKE LOWER(CONCAT('%', :keyword, '%'))"
+    )
+
     List<Product> searchProducts(String keyword);
 }
