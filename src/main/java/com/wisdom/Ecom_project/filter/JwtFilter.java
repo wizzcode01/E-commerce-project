f47c5f119a754e6cd0af710b4139a8c778391588
Wisdom Dickson <dicksonwisdom005@gmail.com>
2026-09-12 17:34:05 +0100

    adding jwt service

--- src/main/java/com/wisdom/Ecom_project/filter/JwtFilter.java
+++ src/main/java/com/wisdom/Ecom_project/filter/JwtFilter.java
@@ -2,13 +2,18 @@ package com.wisdom.Ecom_project.filter;
 
 import com.wisdom.Ecom_project.service.MyUserDetailsService;
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 public class JwtFilter extends OncePerRequestFilter {
 
@@ -16,20 +21,26 @@ public class JwtFilter extends OncePerRequestFilter {
     ApplicationContext context;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain){
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String authHeader = request.getHeader("Authorization");
         String token = null;
         String userEmail = null;
 
-        if(authHeader != null && authHeader.startsWith("Bearer ")){
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
             token = authHeader.substring(7);
             userEmail = jwtService.extractUserEmail(token);
         }
 
-        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = context.getBean(MyUserDetailsService.class).loadUserByUsername(userEmail);
 
-            if(jwtService.validateToken(token, userDetails)){
-
+            if (jwtService.validateToken(token, userDetails)) {
+                UsernamePasswordAuthenticationToken authtoken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authtoken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authtoken);
             }
         }
+        filterChain.doFilter(request, response);
+    }
+
+}
---
+++ src/main/java/com/wisdom/Ecom_project/service/jwtService.java
@@ -0,0 +1,53 @@
+package com.wisdom.Ecom_project.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class jwtService {
+
+    private String secretKey = "";
+    public jwtService(){
+        try{
+            KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
+            SecretKey sk = keyGen.generateKey();
+            secretKey = Base64.getEncoder().encodeToString(sk.getEncoded());
+        }catch(NoSuchAlgorithmException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String generateToken(String userEmail){
+        Map<String, Object> claims = new HashMap<>();
+
+        return Jwts.builder()
+                .claims()
+                .add(claims)
+                .subject(userEmail)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 60 * 60 *30))
+                .and()
+                .signWith(getKey())
+                .compact();
+    }
+
+    private SecretKey getKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String extractUserEmail(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+}
