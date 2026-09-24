4129ae802d9ae8d111d8fd8549f4b8a75683905d
Wisdom Dickson <dicksonwisdom005@gmail.com>
2026-09-24 07:42:55 +0100

    added transaction wallet model with enum for transaction type

---
+++ src/main/java/com/wisdom/Ecom_project/model/TransactionType.java
@@ -0,0 +1,6 @@
+package com.wisdom.Ecom_project.model;
+
+public enum TransactionType {
+    CREDIT,
+    DEBIT
+}
---
+++ src/main/java/com/wisdom/Ecom_project/model/WalletTransaction.java
@@ -0,0 +1,42 @@
+package com.wisdom.Ecom_project.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "wallet_transactions")
+public class WalletTransaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "wallet_id", nullable = false)
+    private Wallet wallet;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TransactionType type;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    public WalletTransaction() {}
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = Instant.now();
+    }
+
+
+
+}
