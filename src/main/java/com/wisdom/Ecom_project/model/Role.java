8973b703e2cb85c007f682520fa17cce218f2f43
Wisdom Dickson <dicksonwisdom005@gmail.com>
2026-09-24 02:57:41 +0100

    adding rbac

--- src/main/java/com/wisdom/Ecom_project/model/Role.java
+++ src/main/java/com/wisdom/Ecom_project/model/Role.java
@@ -1,7 +1,11 @@
 package com.wisdom.Ecom_project.model;
 
 public enum Role {
-    CUSTOMER,
-    RETAILER,
-    ADMIN
+    ROLE_CUSTOMER,
+    ROLE_FOOD_VENDORS,
+    ROLE_HOME_SERVICES,
+    ROLE_RIDERS,
+    ROLE_DRY_CLEANING,
+    ROLE_ELECTRONICS,
+    ROLE_ADMIN
 }
